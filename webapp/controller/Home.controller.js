ab8cca33ccbe8c10871eeb007bce0fa79054730c
sjagmeet37 <sjagmeet37@gmail.com>
2019-10-08 09:19:22 +0530

    unit1 over

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
+	"../model/formatter",
+	"sap/m/MessageToast"
+], function(Controller, formatter, MessageToast) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
@@ -10,6 +11,11 @@ sap.ui.define([
 
 		onInit: function () {
 
+		},
+		onButtonPress : function(oEvent){
+			var title = this.getView().getModel("sample").getProperty("/glossary/title");
+			MessageToast.show("Hello!" + title);
 		}
+
 	});
 });
